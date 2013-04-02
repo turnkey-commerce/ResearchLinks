776e9cef8996b820baa828816eaaa74e15312ddc
James Culbertson <info@turnkey-commerce.com>
2013-04-02 18:46:39 -0500

    - Implement Angular controller and view for projects. Add JS libraries.

---
+++ ResearchLinks/Scripts/projectService.js
@@ -0,0 +1,8 @@
+﻿angular.module("projectService", ["ngResource"]).
+    factory("Project", function ($resource) {
+        return $resource(
+            "/api/projects/:Id",
+            { Id: "@Id" },
+            { "update": { method: "PUT"} }
+        );
+    });
---
+++ ResearchLinks/Scripts/researchLinks.js
@@ -0,0 +1,21 @@
+﻿angular.module('researchLinks', ['projectService', 'ui.bootstrap'])
+    .config(function ($routeProvider) {
+    $routeProvider.
+      when('/', { controller: ListCtrl, templateUrl: 'projectList.html' }).
+      otherwise({ redirectTo: '/' });
+});
+
+function ListCtrl($scope, Project) {
+    $scope.alerts = [];
+    Project.query({}, function (data) {
+        $scope.projects = data;
+    }, function (response) {
+        if (response.status !== 200) {
+            $scope.alerts.push({ type: 'error', msg: "Error: " + response.message});
+        }
+    });
+
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
